afa7b8aa065a55a4b416395f695bac7e81762b18
johnservo <jaservo@up.edu.ph>
2023-07-22 09:10:29 +0800

    Feat: Can now get Territories

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ const Home = () => {
 
 export default Home;
 
-export const getServerSideProps = (context) => {
+export const getServerSideProps = async (context) => {
+    const BASE_URL = process.env.BASE_URL;
+
+    const res = await fetch(`${BASE_URL}/Territories/All`);
+    const { data } = await res.json();
 
 }
