f798f7f07724843aba4cbed41f8c6e386e2a20d7
johnservo <jaservo@up.edu.ph>
2023-07-22 15:17:39 +0800

    Refactor: Scrapped previous solution as it was impossible to add collapse/expand feature

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,56 @@
-import TerritoryTree from "@/components/TerritoryTree";
+import { useState } from "react"
+
+const TerritoryTree = ({ descendants }) => {
+    return (
+        <ul>
+            {descendants.map((t) => (
+                <li key={t.id}>
+                    {t.name}
+                    {t.children && (
+                        <TerritoryTree descendants={t.children} />
+                    )}
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+const Territory = ({ name, descendants }) => {
+    const [isCollapsed, setCollapsed] = useState(true);
+
+    return (
+        <li>
+            <button onClick={() => setCollapsed(name)}> {name}</button>
+            {descendants &&
+                <ul>
+                    {descendants.map((d) =>
+                        <Territory key={d.id} name={d.name} descendants={d.children} />
+                    )}
+                </ul>
+            }
+        </li>
+    )
+}
 
 const Home = ({ territories }) => {
     console.log(territories)
 
-    return <TerritoryTree descendants={territories} />
+    return (
+        <ul>
+            {territories.map((t) => (
+                <li key={t.id}>
+                    {t.name}
+                    {t.children &&
+                        <ul>
+                            {t.children.map((c) =>
+                                <Territory key={c.id} name={c.name} descendants={c.children} />
+                            )}
+                        </ul>
+                    }
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 export default Home;
