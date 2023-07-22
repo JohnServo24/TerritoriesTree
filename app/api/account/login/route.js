e5a7f8598e7d001dd7050cfe02b63a14780a91bd
johnservo <jaservo@up.edu.ph>
2023-07-22 20:30:24 +0800

    Refactor: Extracted JWT into its own file

--- app/api/account/login/route.js
+++ app/api/account/login/route.js
@@ -1,16 +1,15 @@
 import { cookies } from 'next/dist/client/components/headers';
 import { NextResponse } from 'next/server';
-import jwt from 'jsonwebtoken';
 
 import request from "@/utils/request";
 import { NOT_FOUND } from '@/constants/httpStatuses';
 import InvalidCredentials from '@/errors/InvalidCredentials';
 import serverErrorHandler from '@/utils/serverErrorHandler';
+import { setToken } from '@/utils/jwt';
 
 const BASE_URL = `${process.env.BASE_URL}/Account/SignIn`;
-const JWT_SECRET = process.env.JWT_SECRET;
 
-export async function POST(req, res) {
+export async function POST(req) {
     try {
         const { username, password } = await req.json();
 
@@ -20,7 +19,7 @@ export async function POST(req, res) {
             throw new InvalidCredentials(loginReq.body?.message);
         }
 
-        const token = jwt.sign(loginReq.body, JWT_SECRET)
+        const token = setToken({ username, password });
         cookies().set('token', token, { secure: true });
 
         return NextResponse.json({ message: "Hello" });
---
+++ utils/jwt.js
@@ -0,0 +1,7 @@
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const setToken = (data) => {
+    return jwt.sign(data, JWT_SECRET)
+} 
