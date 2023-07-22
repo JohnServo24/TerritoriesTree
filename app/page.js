ebea9bf76bca4556099ccf70c8a6d9eabbc3f1a5
johnservo <jaservo@up.edu.ph>
2023-07-22 21:30:11 +0800

    Feat: Tokens can now expire

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+import { redirect } from 'next/navigation';
+
 import styles from '@/styles/Home.module.scss'
 import TerritoryTree from '@/components/TerritoryTree';
+import { JWT_COOKIE_NAME } from '@/constants/misc';
 import getTerritories from '@/utils/getTerritories';
 import createHierarchy from '@/utils/createHierarchy';
 import { getCookie } from '@/utils/cookies';
-import { JWT_COOKIE_NAME } from '@/constants/misc';
-import { redirect } from 'next/navigation';
+import { isTokenExpired } from '@/utils/jwt';
 
 const LOGIN_URL = '/account/login';
 
@@ -14,7 +16,9 @@ export default async function Home() {
 
     const token = getCookie(JWT_COOKIE_NAME);
 
-    if (!token) redirect(LOGIN_URL);
+    if (!token || isTokenExpired(token.value)) {
+        redirect(LOGIN_URL);
+    }
 
     return (
         <ul className={styles.territory__list}>
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,7 +1,18 @@
 import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET;
+const TOKEN_DURATION = 14400;
 
 export const setToken = (data) => {
-    return jwt.sign(data, JWT_SECRET);
+    return jwt.sign(data, JWT_SECRET, { expiresIn: TOKEN_DURATION });
 } 
+
+// Didn't use verify because I don't want it to
+// throw an error if the token is not valid.
+export const isTokenExpired = (token) => {
+    if (!token) return null;
+
+    const data = jwt.decode(token, JWT_SECRET);
+
+    return new Date() > new Date(data.exp * 1000);
+}
