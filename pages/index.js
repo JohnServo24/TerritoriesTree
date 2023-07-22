378a2b3899e0e274ab70f44b30798265c0bc73e1
johnservo <jaservo@up.edu.ph>
2023-07-22 10:18:42 +0800

    Chore: Add comment

--- pages/index.js
+++ pages/index.js
@@ -77,3 +77,4 @@ export const getServerSideProps = async () => {
 // GET ALL ROOT NODES
 // From the root nodes, render the children
 // If the children has children, render those children
+// Repeat Repeat Repeat until we get to the bottom
