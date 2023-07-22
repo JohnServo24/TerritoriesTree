aa4ae0d1ce0a699fa9221d9875278c076856eb5c
johnservo <jaservo@up.edu.ph>
2023-07-22 19:03:39 +0800

    Refactor: Improved request handler

--- app/api/account/login/route.js
+++ app/api/account/login/route.js
@@ -1,9 +1,14 @@
 import { cookies } from 'next/dist/client/components/headers';
 import { NextResponse } from 'next/server';
+import request from "@/utils/request";
+
+const BASE_URL = `${process.env.BASE_URL}/Account/SignIn`;
 
 export async function POST(req, res) {
     const { username, password } = await req.json();
 
+    const loginData = await request.post(BASE_URL, { username, password })
+    console.log(loginData)
 
     return NextResponse.json({ message: "Hello" });
 }
--- utils/request.js
+++ utils/request.js
@@ -5,24 +5,44 @@ class Request {
         }
     }
 
+    async generateReturnValue(res) {
+        const data = {
+            code: res.status,
+            text: res.statusText,
+            body: await res.json(),
+        }
+
+        if (res.status >= 400) {
+            return {
+                status: 'error',
+                ...data
+            }
+        }
+
+        return {
+            status: 'success',
+            ...data
+        }
+    }
+
     async get(url, params = {}) {
-        const resRaw = await fetch(url, {
+        const res = await fetch(url, {
             ...this.defaultParams,
             ...params
         });
 
-        return await resRaw.json();
+        return this.generateReturnValue(res)
     }
 
-    async post(url, body, params) {
-        const resRaw = await fetch(url, {
+    async post(url, body, params = {}) {
+        const res = await fetch(url, {
             method: "POST",
             body: JSON.stringify(body),
             ...this.defaultParams,
             ...params
-        })
+        });
 
-        return await resRaw.json();
+        return this.generateReturnValue(res);
     }
 }
 
