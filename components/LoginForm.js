d0d7073ef40e28dcb44a4d04a7dda5e428083ce6
johnservo <jaservo@up.edu.ph>
2023-07-22 16:12:47 +0800

    Feat: Can now record username and password

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     const onSubmit = (e) => {
-        console.log(onSubmit);
+        e.preventDefault();
     }
 
     return (
         <form onSubmit={onSubmit}>
-            Username: <input />
-            Password: <input />
+            Username:
+            <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            Password:
+            <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
 
             <button type="submit">Submit</button>
         </form>
