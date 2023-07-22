ec6cc09b785a5cabba7ccca92dbecd0ef60c16f4
johnservo <jaservo@up.edu.ph>
2023-07-22 09:11:13 +0800

    Feat: Can now pass territories to front end

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
-const Home = () => {
+const Home = ({ territories }) => {
+    console.log(territories)
 
 }
 
 export default Home;
 
-export const getServerSideProps = async (context) => {
+export const getServerSideProps = async () => {
     const BASE_URL = process.env.BASE_URL;
 
     const res = await fetch(`${BASE_URL}/Territories/All`);
     const { data } = await res.json();
 
+    return {
+        props: {
+            territories: data,
+        }
+    }
 }
