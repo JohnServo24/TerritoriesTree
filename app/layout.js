a3f83bdc5ae32bbc8a2cd758a99f6b0b086fc44a
johnservo <jaservo@up.edu.ph>
2023-07-23 18:18:50 +0800

    Chore: Centered everything

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.scss'
+import styles from '@/styles/MainLayout.module.scss'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +13,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <main>
+                <main className={styles["main-layout"]}>
                     {children}
                 </main>
             </body>
