ffea94f91b4c079bb5724ce31d1d8c9a52b35029
johnservo <jaservo@up.edu.ph>
2023-07-23 13:59:56 +0800

    Chore: Secured cookies

--- utils/cookies.js
+++ utils/cookies.js
@@ -1,7 +1,13 @@
 import { cookies } from 'next/headers';
 
 export const setCookie = (name, value) => {
-    cookies().set(name, value, { secure: true });
+    cookies().set(name, value, { 
+        secure: true,
+        httpOnly: true,
+        sameSite: "strict",
+        path: '/',
+        maxAge: 14400
+    });
 }
 
 export const getCookie = (name) => {
